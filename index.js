6030429dc1c3822892755234a9216f873bad28d6
SamHarron <harronsam@gmail.com>
2021-08-30 22:03:08 -0400

    commit tree 467833b06b1fc7cc62eef12c883b769ae13fe82e

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 16;
+const num4 = 6;
+
+const mod = num3 % num4;
+
+const max = Math.max ( 1, 20, 5, 2, 8, 9, 7, 15);
